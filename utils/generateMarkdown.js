7e7f8aa12f58bb91dea01dec8cedba9e4f2ca66c
Sean Hayes <hayessea000@outlook.com>
2023-04-24 16:04:02 -0500

    set up generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,8 +13,52 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+  
+  ${data.description}
+  
 
-`;
+  ## Table of Contents (Optional)
+  
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+
+  ## Usage
+  
+  ${data.usage}
+  
+
+  ## Credits
+  
+  ${data.contributing}
+  
+
+  ## License
+  
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  
+  
+  ## Tests
+  
+  ${data.tests}
+  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+  
+  
+  ## Questions
+
+  ${data.username}
+  ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
